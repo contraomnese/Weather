ecf4fa820cdcad0e56ed94c752dccd37c399b3ae
Sergey Kurbatov <k-s-s95@mail.ru>
2025-07-15 20:38:54 +0300

    Added theme - colorScheme, typography, dimensions

---
+++ core/design/src/main/java/com/contraomnese/weather/design/Color.kt
@@ -0,0 +1,28 @@
+package com.arbuzerxxl.vibeshot.core.design.theme
+
+import androidx.compose.ui.graphics.Color
+
+val RichCoral = Color(0xFFFF3B2F)
+val DarkTurquoise = Color(0xFF0095A8)
+val DarkToneInk = Color(0xFF121212)
+val CarbonFiber = Color(0xFF1E1E1E)
+val Carbon = Color(0xFF2D2D2D)
+val TangledWeb = Color(0xFFB3B3B3)
+val FluorescentRed = Color(0xFFFF5252)
+val RichRed = Color(0xFFFF1744)
+val ScreenGlow = Color(0xFF69F0AE)
+
+val CoralPink = Color(0xFFFF6F61)
+val CoralLight = Color(0xFFCCC2DC)
+val DarkBurgundy = Color(0xFF410002)
+val Turquoise = Color(0xFF00B8D4)
+val TurquoiseLight = Color(0xFFA6EEFF)
+val StellarExplorer = Color(0xFF001F26)
+val Brilliance = Color(0xFFFDFDFD)
+val EerieBlack = Color(0xFF1A1A1A)
+val GrayLight = Color(0xFFF5F5F5)
+val Gray = Color(0xFF444444)
+val MutedRed = Color(0xFFBA1A1A)
+val ForgottenPink = Color(0xFFFFDAD6)
+val Maroon = Color(0xFF410002)
+val Green = Color(0xFF2E7D32)
---
+++ core/design/src/main/java/com/contraomnese/weather/design/Dimen.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2023. Kiparo.ru
+ */
+
+package com.arbuzerxxl.vibeshot.core.design.theme
+
+import androidx.compose.ui.unit.dp
+
+val circleItemDiameter = 264.dp
+val circleImageItemHeight = 202.dp
+val circleImageItemWidth = 232.dp
+
+val padding2 = 2.dp
+val padding4 = 4.dp
+val padding8 = 8.dp
+val padding12 = 12.dp
+val padding16 = 16.dp
+val padding20 = 20.dp
+val padding24 = 24.dp
+val padding28 = 28.dp
+val padding32 = 32.dp
+val padding36 = 36.dp
+val padding40 = 40.dp
+val padding80 = 80.dp
+val padding160 = 160.dp
+
+val bottomSheetHiddenOffset = 120.dp
+
+val itemWidth0_5 = 0.5.dp
+val itemWidth1 = 1.dp
+val itemWidth2 = 2.dp
+val itemWidth3 = 3.dp
+
+val itemHeight2 = 2.dp
+val itemHeight4 = 4.dp
+val itemHeight16 = 16.dp
+val itemWidth40 = 40.dp
+
+val itemHeight24 = 24.dp
+val itemWidth24 = 24.dp
+
+val itemHeight30 = 30.dp
+
+val itemHeight40 = 40.dp
+val itemWidth216 = 216.dp
+
+val itemHeight75 = 75.dp
+val itemWidth104 = 104.dp
+
+val itemHeight48 = 48.dp
+val itemWidth48 = 48.dp
+
+val itemHeight52 = 52.dp
+
+val itemHeight64 = 64.dp
+val itemWidth64 = 64.dp
+
+val itemHeight96 = 96.dp
+val itemHeight128 = 128.dp
+val itemWidth140 = 140.dp
+
+val itemSize136 = 136.dp
+
+val itemWidth80 = 80.dp
+val itemHeight160 = 160.dp
+
+val cornerRadius6 = 6.dp
+val cornerRadius8 = 8.dp
+val cornerRadius16 = 16.dp
+val cornerRadius32 = 32.dp
+
+val space8 = 8.dp
+val space16 = 16.dp
+val space24 = 24.dp
+val space32 = 32.dp
+val space40 = 40.dp
+val space56 = 56.dp
+
+val zeroValue = 0.dp
+
+val elevation4 = 4.dp
+
+val zero = 0.dp
+
+val cornerSize2 = 2.dp
+val cornerSize8 = 8.dp
+val cornerSize16 = 16.dp
+val cornerSize28 = 28.dp
+
+val baseButtonHeight = 40.dp
+
+val shadow_section_product = 4.dp
+
+val size_welcome_background_circle = 264.dp
