0be06e0f780140e7ffac257af77c415902c5b2ad
Sergey Kurbatov <k-s-s95@mail.ru>
2025-08-05 14:36:42 +0300

    Add Home feature.

--- features/home/build.gradle.kts
+++ features/home/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
@@ -7,20 +7,15 @@ plugins {
 }
 
 android {
-    namespace = "com.contraomnese.weather"
+    namespace = "com.contraomnese.weather.home"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.contraomnese.weather"
         minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-        android.buildFeatures.buildConfig = true
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
 
-    buildFeatures.buildConfig = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
 
     buildTypes {
         release {
@@ -49,29 +44,16 @@ tasks.withType<Test>().configureEach {
 
 dependencies {
 
-    // modules
     implementation(project(":core:ui"))
-    implementation(project(":core:design"))
     implementation(project(":core:presentation"))
-    implementation(project(":data"))
     implementation(project(":domain"))
 
-    implementation(project(":features:home"))
-
-    implementation(libs.material)
     implementation(libs.bundles.koin)
     implementation(libs.bundles.core)
     implementation(libs.bundles.navigation)
     implementation(libs.bundles.presentation)
     implementation(libs.bundles.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.bundles.ui)
-    implementation(libs.bundles.network)
-    implementation(libs.bundles.room)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.bundles.datastore)
-
-    implementation(libs.androidx.core.splashscreen)
 
     testImplementation(libs.bundles.test)
     testRuntimeOnly(libs.junit.jupiter.engine)
