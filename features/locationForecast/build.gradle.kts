0726ae347d82b17c908e3e1be7acc58b71a3f7db
Sergey Kurbatov <k-s-s95@mail.ru>
2025-08-06 15:49:56 +0300

    Add new feature locationForecast.

--- features/locationForecast/build.gradle.kts
+++ features/locationForecast/build.gradle.kts
@@ -1,40 +1,24 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.devtools.ksp)
 }
 
-val WEATHER_API_KEY: String = gradleLocalProperties(rootDir, providers).getProperty("WEATHER_API_KEY")
-val WEATHER_API_BASE_URL: String = gradleLocalProperties(rootDir, providers).getProperty("WEATHER_API_BASE_URL")
-
 android {
-    namespace = "com.contraomnese.weather"
+    namespace = "com.contraomnese.weather.locationforecast"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.contraomnese.weather"
         minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-        android.buildFeatures.buildConfig = true
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildFeatures.buildConfig = true
-
     buildTypes {
-        debug {
-            buildConfigField("String", "WEATHER_API_KEY", "\"${WEATHER_API_KEY}\"")
-            buildConfigField("String", "WEATHER_API_BASE_URL", "\"${WEATHER_API_BASE_URL}\"")
-        }
         release {
-            buildConfigField("String", "WEATHER_API_KEY", "\"${WEATHER_API_KEY}\"")
-            buildConfigField("String", "WEATHER_API_BASE_URL", "\"${WEATHER_API_BASE_URL}\"")
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
@@ -60,30 +44,17 @@ tasks.withType<Test>().configureEach {
 
 dependencies {
 
-    // modules
     implementation(project(":core:ui"))
     implementation(project(":core:design"))
     implementation(project(":core:presentation"))
-    implementation(project(":data"))
     implementation(project(":domain"))
 
-    implementation(project(":features:home"))
-    implementation(project(":features:locationForecast"))
-
-    implementation(libs.material)
     implementation(libs.bundles.koin)
     implementation(libs.bundles.core)
     implementation(libs.bundles.navigation)
     implementation(libs.bundles.presentation)
     implementation(libs.bundles.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.bundles.ui)
-    implementation(libs.bundles.network)
-    implementation(libs.bundles.room)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.bundles.datastore)
-
-    implementation(libs.androidx.core.splashscreen)
 
     testImplementation(libs.bundles.test)
     testRuntimeOnly(libs.junit.jupiter.engine)
