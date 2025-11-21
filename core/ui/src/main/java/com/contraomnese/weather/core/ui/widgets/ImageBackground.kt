e868d8a18673b5efc6121c8f3885991e466dab39
Sergey Kurbatov <k-s-s95@mail.ru>
2025-11-21 13:32:07 +0300

    Fix memory leak in ImageBackground and replace images

--- core/ui/src/main/java/com/contraomnese/weather/core/ui/widgets/ImageBackground.kt
+++ core/ui/src/main/java/com/contraomnese/weather/core/ui/widgets/ImageBackground.kt
@@ -6,13 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.contraomnese.weather.design.R
 import com.contraomnese.weather.design.theme.WeatherTheme
@@ -23,14 +20,9 @@ fun ImageBackground(
     modifier: Modifier = Modifier,
     @DrawableRes backgroundResId: Int,
 ) {
-    val context = LocalContext.current
-
-    val imageBitmap = remember(backgroundResId) {
-        ImageBitmap.imageResource(context.resources, backgroundResId)
-    }
 
     Image(
-        bitmap = imageBitmap,
+        painter = painterResource(backgroundResId),
         contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = modifier
